74be449eba95e45a9695d6e3107075641d2790f3
Nikhil I <56198218+unikill066@users.noreply.github.com>
2025-05-09 07:31:56 -0500

    Delete agentic_web_crawler directory

--- agentic_web_crawler/agentic_parser.py
+++
@@ -1,6 +0,0 @@
-from crew_config import build_crew
-import sys, asyncio
-
-if __name__ == "__main__":
-    keyword = " ".join(sys.argv[1:]) or input("Keyword: ")
-    asyncio.run(build_crew(keyword).run())
--- agentic_web_crawler/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-# tools/__init__.py
-from .labcorp_tool import one_page
